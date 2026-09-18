af31d49925b103084c0b6d2fa8030fc244ed5dda
Jahonqulov Rustam <jahonqulovr@gmail.com>
2026-09-19 00:39:59 +0500

    Simplify main method to print 'Hello'
    Removed user input handling and subsequence check.

--- Main.java
+++ Main.java
@@ -5,36 +5,6 @@ import java.util.*;
 class Main{
 	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int count = 0;
-		String s = sc.nextLine();
-		int n = Integer.parseInt(sc.nextLine());
-		
-		for (int i = 0; i < n; i++) {
-			String word = sc.nextLine();
-			
-			if (isSubSequenceOfS(s, word)) count++;
-		}
-		
-		
-		System.out.println(count);
+		System.out.print("Hello");
 	}
-	
-	public static boolean isSubSequenceOfS(String s, String word) {
-		
-		if (word.length() > s.length()) return false;
-		
-		int index = -1;
-		
-		for (int i = 0; i < word.length(); i++) {
-			index = s.indexOf(word.charAt(i), index +1);
-			if (index == -1) {
-				return false;
-			}
-		}
-		
-		return true;
-	}
-	
 }
